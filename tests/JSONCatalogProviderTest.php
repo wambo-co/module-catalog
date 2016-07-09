7fc1ee3cd2c6a2fa259d8cb8641b8fcb1eedbb6a
Andreas Koch <andyk7@gmail.com>
2016-07-09 23:38:47 +0200

    Add an integration test for the catalog provider

--- tests/JSONCatalogProviderTest.php
+++ tests/JSONCatalogProviderTest.php
@@ -5,7 +5,10 @@ use League\Flysystem\Memory\MemoryAdapter;
 use Wambo\Catalog\Error\CatalogException;
 use Wambo\Catalog\JSONCatalogProvider;
 use Wambo\Catalog\Mapper\CatalogMapper;
+use Wambo\Catalog\Mapper\ProductMapper;
 use Wambo\Catalog\Model\Catalog;
+use Wambo\Catalog\Validation\SKUValidator;
+use Wambo\Catalog\Validation\SlugValidator;
 
 /**
  * Class JSONCatalogTest tests the JSONCatalog class.
@@ -109,5 +112,50 @@ JSON;
 
         // assert
         $this->assertNotNull($catalog, "getCatalog should return a catalog if the mapper returned one");
+    }
+
+    /**
+     * Integration test
+     * @test
+     */
+    public function getAllProducts_IntegrationTest_CatalogWithProductsIsReturned()
+    {
+        // arrange
+        $filesystem = new Filesystem(new MemoryAdapter());
+        $skuValidator = new SKUValidator();
+        $slugValidator = new SlugValidator();
+        $productMapper = new ProductMapper($skuValidator, $slugValidator);
+        $catalogMapper = new CatalogMapper($productMapper);
+
+        $catalogJSON = <<<JSON
+[
+    {
+        "sku": "t-shirt-no-1",
+        "slug": "t-shirt-no-1",
+        "title": "T-Shirt No. 1",
+        "summary": "Our T-Shirt No. 1"
+    },
+    {
+        "sku": "t-shirt-no-2",
+        "slug": "t-shirt-no-2",
+        "title": "T-Shirt No. 2",
+        "summary": "Our T-Shirt No. 2"
+    },
+    {
+        "sku": "t-shirt-no-3",
+        "slug": "t-shirt-no-3",
+        "title": "T-Shirt No. 3",
+        "summary": "Our T-Shirt No. 3"
+    }
+]
+JSON;
+        $filesystem->write("catalog.json", $catalogJSON);
+        $jsonCatalogProvider = new JSONCatalogProvider($filesystem, "catalog.json", $catalogMapper);
+
+        // act
+        $catalog = $jsonCatalogProvider->getCatalog();
+
+        // assert
+        $this->assertCount(3, $catalog, "getCatalog should return a catalog with 3 products");
     }
 }
